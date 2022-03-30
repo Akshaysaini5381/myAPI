a8a766e472ccb50d73d3f3ad8766c15b68d8aa50
admin <admin@CHETUIWK385>
2022-03-30 14:27:21 +0530

    Add project files.

---
+++ Controllers/empController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace myAPI.Controllers
+{
+    public class empController : ApiController
+    {
+      [HttpGet]
+      public List<string>getemp()
+        {
+            List<string> obj = new List<string>()
+            {
+
+
+                "akshay saini",
+                "ashi saini",
+                "saini sarkar"
+
+
+            };
+            return obj;
+        }
+
+        [HttpPost]
+        public List<string>save()
+        {
+            List<string> sobj = new List<string>();
+            return sobj;
+        }
+    }
+}
